c37919e90ef1a860c00c7ee74d6c983d38e0223a
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-08-12 23:42:44 -0400

    gallery UI on orientation change

--- components/PhotoGallery.js
+++ components/PhotoGallery.js
@@ -1,30 +1,58 @@
-import { FlatList, Image, Text, StyleSheet, Dimensions } from "react-native"
+import { useEffect, useState } from "react";
+import { FlatList, Image, Text, StyleSheet, Dimensions, useWindowDimensions } from "react-native";
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 const PhotoGallery = ({ cameraRoll, setViewGallery }) => {
-    const renderImageItem = ({item}) => {
-        const imgSize = Dimensions.get('window').width / 4
+    const [numColumns, setNumColumns] = useState(4);
+    const [imgSize, setImgSize] = useState(Dimensions.get('window').width / 4);
+    const [orientation, setOrientation] = useState('portrait');
+
+    useEffect(() => {
+        const handleOrientationChange = async () => {
+            const { width, height } = Dimensions.get('window');
+            const currentOrientation = width > height ? 'landscape' : 'portrait';
+            setOrientation(currentOrientation);
+            if (currentOrientation === 'landscape') {
+                setNumColumns(6);
+            } else {
+                setNumColumns(4);
+            }
+            setImgSize(width / numColumns);
+        };
+
+        const subscription = ScreenOrientation.addOrientationChangeListener(handleOrientationChange);
+        handleOrientationChange();
+
+        return () => {
+            ScreenOrientation.removeOrientationChangeListener(subscription);
+        };
+    
+    }, [numColumns]);
+
+    const renderImageItem = ({ item }) => {
         return (
-            <Image 
-                source={{ uri: item}}
-                width={imgSize}
-                height={imgSize}
+            <Image
+                source={{ uri: item }}
+                style={{ width: imgSize, height: imgSize }}
             />
         )
     }
-    
+
     return (
         <FlatList
             data={cameraRoll}
             renderItem={renderImageItem}
             keyExtractor={(item, index) => index.toString()}
-            numColumns={4}               
+            numColumns={numColumns}
+            key={`${numColumns}-${imgSize}`}
             ListHeaderComponent={
-                <>
-                    <Text 
+                <Text
                     style={styles.x}
-                    onPress={() => setViewGallery(false)}>X</Text>
-                </>
+                    onPress={() => setViewGallery(false)}>
+                    X
+                </Text>
             }
+            extraData={imgSize}
         />
     )
 }
@@ -35,7 +63,7 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         alignSelf: 'flex-end',
         padding: 50
-    }
+    },
 })
 
 export default PhotoGallery;
